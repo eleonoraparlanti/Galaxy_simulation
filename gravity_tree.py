Fixed normalization on tree
the tree test notebook shows how to use the new normalization procedure

gravity_tree.py
@@ -15,6 +15,7 @@ from numpy import int64   as NP_INT
 from numpy import float64 as NP_FLOAT
 
 import numpy as np
+import matplotlib.pyplot as plt
 from numba.experimental import jitclass
 from numba import njit
 
@@ -49,7 +50,7 @@ spec_tree = {
 @jitclass(spec_tree)
 class tree:
 
-    def __init__(self,n_dim=2,n_grid = 10,n_part_per_cell=1, mass_pt=np.zeros(2),pos_pt=np.zeros((2,2))):
+    def __init__(self, n_dim=2, n_grid = 10, n_part_per_cell=1, mass_pt=np.zeros(2), pos_pt=np.zeros((2,2))):
 
         self.n_dim           = n_dim
         self.n_children      = int(2**n_dim)
@@ -128,7 +129,7 @@ class tree:
 def compute_mass_on_tree(tree_in, i_iter_max=100):
 
   """
-  once tree has bee build, particle mass is assigned to the tree
+  once the tree has been built, particle mass is assigned to the tree
   """
 
   # assign mass to the leaves
@@ -164,8 +165,8 @@ def build_tree_from_particles(tree_in,i_iter_max=100):
   """
 
   # init ids for particles to be assigne
-  ids_parts_to_assign     = np.zeros(tree_in.n_pt,dtype=NP_INT)
-  ids_parts_to_assign_new = np.zeros(tree_in.n_pt,dtype=NP_INT)
+  ids_parts_to_assign     = np.zeros(tree_in.n_pt, dtype=NP_INT)
+  ids_parts_to_assign_new = np.zeros(tree_in.n_pt, dtype=NP_INT)
   n_pt_to_assign          = tree_in.n_pt
   n_pt_to_assign_new      = tree_in.n_pt
   for i_pt in range(n_pt_to_assign):
@@ -224,8 +225,8 @@ def compute_potential_tree(tree_in,critical_angle=0.0, G_gravity= 1.0, n_iter_ma
   pos_j_part     = np.zeros(tree_in.n_dim)
 
   # lists
-  id_cells_to_check     = np.zeros(tree_in.n_max,dtype=NP_INT)
-  id_cells_to_check_new = np.zeros(tree_in.n_max,dtype=NP_INT)
+  id_cells_to_check     = np.zeros(tree_in.n_max, dtype=NP_INT)
+  id_cells_to_check_new = np.zeros(tree_in.n_max, dtype=NP_INT)
   n_cells_to_check      = 0
   n_cells_to_check_new  = 0
 
@@ -236,11 +237,11 @@ def compute_potential_tree(tree_in,critical_angle=0.0, G_gravity= 1.0, n_iter_ma
 
   if verbose:
     print("calling compute_potential_tree()")
-    print("  particle number            ",tree_in.n_pt)
-    print("  number of dimensions       ",tree_in.n_dim)
-    print("  number of cells            ",tree_in.n_now)
-    print("  max level                  ",np.max(tree_in.levels))
-    print("  opening angle              ",critical_angle)
+    print("  particle number            ", tree_in.n_pt)
+    print("  number of dimensions       ", tree_in.n_dim)
+    print("  number of cells            ", tree_in.n_now)
+    print("  max level                  ", np.max(tree_in.levels))
+    print("  opening angle              ", critical_angle)
 
   # loop on the particle
   for i_part in range(tree_in.n_pt):
@@ -316,10 +317,10 @@ def compute_potential_tree(tree_in,critical_angle=0.0, G_gravity= 1.0, n_iter_ma
       n_approx_tot= n_approx_tot + float(i_approx)
   
   if verbose:
-    print("  average iterations         ",n_iter_tot/tree_in.n_pt)
-    print("  average exact  calculations",n_exact_tot/tree_in.n_pt)
-    print("  average approx calculations",n_approx_tot/tree_in.n_pt)
-    print("  theoretical speedup        ",tree_in.n_pt**2/n_iter_tot)
+    print("  average iterations         ", n_iter_tot/tree_in.n_pt)
+    print("  average exact  calculations", n_exact_tot/tree_in.n_pt)
+    print("  average approx calculations", n_approx_tot/tree_in.n_pt)
+    print("  theoretical speedup        ", tree_in.n_pt**2/n_iter_tot)
 
   out[:] = out[:] * G_gravity
 
@@ -339,8 +340,8 @@ def compute_acceleration_tree(tree_in,critical_angle=0.0, G_gravity= 1.0, n_iter
   acc_j          = np.zeros(tree_in.n_dim)
   
   # lists
-  id_cells_to_check     = np.zeros(tree_in.n_max,dtype=NP_INT)
-  id_cells_to_check_new = np.zeros(tree_in.n_max,dtype=NP_INT)
+  id_cells_to_check     = np.zeros(tree_in.n_max, dtype=NP_INT)
+  id_cells_to_check_new = np.zeros(tree_in.n_max, dtype=NP_INT)
   n_cells_to_check      = 0
   n_cells_to_check_new  = 0
 
@@ -445,6 +446,87 @@ def compute_acceleration_tree(tree_in,critical_angle=0.0, G_gravity= 1.0, n_iter
 
   return out
 
+@njit
+def normalize_field(n_dim, test_pos):
+    """
+    Returns normalized field and normalization factor
+    """
+    norm_vector = np.ones(n_dim)
+    for i_dim in range(n_dim):
+        norm_vector[i_dim] = np.abs(np.max(test_pos[i_dim,:]) - np.min(test_pos[i_dim,:]))
+        test_pos[i_dim,:] = (test_pos[i_dim,:]-np.min(test_pos[i_dim,:]))\
+            /(np.max(test_pos[i_dim,:])-np.min(test_pos[i_dim,:])) - 0.5
+    return test_pos, norm_vector
+
+# @njit
+def init_particle_field(n_part_per_cell=6, n_dim=2, n_grid=200, iter_max=200, n_pt=320):
+
+    #--------------------------------
+    # set up particles
+    #--------------------------------
+    test_pos  = np.zeros((n_dim,n_pt))
+    test_mass = np.ones(n_pt)
+
+    for i_dim in range(n_dim):
+        test_pos[i_dim,:] = np.random.normal(loc=0.0,scale=1.0, size=n_pt)
+
+    test_pos, norm_vector = normalize_field(n_dim, test_pos)
+    return test_pos, test_mass, n_part_per_cell, n_dim, n_grid, iter_max, n_pt, norm_vector
+
+# @njit
+def init_trees(n_dim, n_grid, n_part_per_cell, test_mass, test_pos, iter_max, critical_angle):
+    # produce test tree with masses
+    test_tree = tree(n_dim=n_dim, n_grid=n_grid, n_part_per_cell=n_part_per_cell, \
+                    mass_pt=test_mass, pos_pt=test_pos)
+    test_tree   = build_tree_from_particles(tree_in=test_tree, i_iter_max=iter_max)
+    test_tree   = compute_mass_on_tree(tree_in=test_tree)
+
+    potential_tree = compute_potential_tree(tree_in=test_tree,critical_angle=critical_angle)
+    
+    return test_tree, potential_tree
+
+def plot_tree(test_tree, test_pos, norm_vector):
+    #--------------------------------
+    # visualize the tree
+    #--------------------------------
+    fig = plt.figure()
+    ax  = fig.add_subplot(111)
+
+    for i_cell in range(test_tree.n_now):
+
+        col    = 'k'
+        zorder = 0
+
+        dx    = 0.5**(test_tree.levels[i_cell]+1) * norm_vector[0]
+        dy    = 0.5**(test_tree.levels[i_cell]+1) * norm_vector[1]
+
+        tree_center_x = test_tree.center[0] - test_tree.center[0].mean()
+        tree_center_y = test_tree.center[1] - test_tree.center[1].mean()
+        # determination of the edges of the square can be cleaned
+        xedge = [tree_center_x[i_cell]*norm_vector[0] - dx, tree_center_x[i_cell]*norm_vector[0] + dx,\
+                tree_center_x[i_cell]*norm_vector[0] + dx,\
+                tree_center_x[i_cell]*norm_vector[0] - dx,\
+                tree_center_x[i_cell]*norm_vector[0] - dx]
+        yedge = [tree_center_y[i_cell]*norm_vector[1] + dy, tree_center_y[i_cell]*norm_vector[1] + dy,\
+                tree_center_y[i_cell]*norm_vector[1] - dy,\
+                tree_center_y[i_cell]*norm_vector[1] - dy,
+                tree_center_y[i_cell]*norm_vector[1] + dy]
+        
+        ax.plot(tree_center_x[i_cell]*norm_vector[0], tree_center_y[i_cell]*norm_vector[1], marker='o', color=col, zorder=zorder)
+
+        ax.plot(xedge,yedge,ls='-',marker='',color=col,zorder=zorder)
+
+    ax.plot((test_pos[0,:]-test_pos[0,:].mean())*norm_vector[0], \
+            (test_pos[1,:]-test_pos[1,:].mean())*norm_vector[1], ls='', marker='x', color='b', alpha=0.6)
+
+@njit
+def normal_to_physical(n_dim, acceleration_tree, test_pos, norm_vector):
+    for i_dim in range(n_dim):
+            acceleration_tree[i_dim] /= norm_vector[i_dim]**2
+            test_pos[i_dim] *= norm_vector[i_dim]
+            test_pos[i_dim] -= test_pos[i_dim].mean()
+    return acceleration_tree, test_pos
+
 if __name__ == "__main__":
 
   """
@@ -466,7 +548,7 @@ if __name__ == "__main__":
   #--------------------------------
   # set up particles
   #--------------------------------
-  n_pt      = 32
+  n_pt      = 320
   test_pos  = np.zeros((n_dim,n_pt))
   test_mass = np.ones(n_pt)
 