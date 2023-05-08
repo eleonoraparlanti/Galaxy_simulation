8822adc2b945c511ed2cf4bb1e332018c9807552
Maulik Bhatt <48092351+maulikthebhatt@users.noreply.github.com>
2023-05-08 19:05:59 +0200

    Add files via upload
    Galaxy evolution with normalization

--- evolve_galaxy.py
+++ evolve_galaxy.py
@@ -18,19 +18,31 @@ if __name__ == "__main__":
   unit_m  = constants.M_sun
   unit_l  = constants.pc
   unit_t = (constants.G * unit_m/unit_l**3)**(-0.5)
-
+  
+  # For normalization
+  pos_normalization  = 20000 
+  vel_normalization  = (units.km/units.s).to(units.pc/units.Myr)
+  
   # system setup
-  M_tot_msun = 1e5   # msun
-  r_0_pc     = 100.0 # pc
+  M_tot_msun      = 1e10   # msun
+  r_0_pc          = 100.0 # pc
   N_pt            = int(1e2)
   N_pt_star       = int(1e2)
-  N_pt_dm         = int(1e2)
+  N_pt_dm         = int(1e1)
   # N_pt       = int(8e2)   for 3d check  
   
   n_grid = 10000
   n_part_per_cell = 8
   n_dim = 3
-
+  
+  # time steps 
+  dt_myr     = 0.01   # in 'Myr'
+  t_end_myr  = 2.0    # in 'Myr'
+  f_snap_myr = 10 
+  
+  G_gravity  = 0.0044985022   ## In unit of (pc^3/Msun/Myr/Myr)
+  G_gravity  = G_gravity  / pos_normalization / pos_normalization / pos_normalization  ## (  " / (2 * r_cut_star) "  ) is mormalization for the position (see pos_normed_star) 
+    
   ############################################################
   #### Reading Initial condition from the file 
   ############################################################
@@ -41,9 +53,10 @@ if __name__ == "__main__":
   x_pos_star     = ic_star[:,1]
   y_pos_star     = ic_star[:,2]
   z_pos_star     = ic_star[:,3]
-  vx_star        = ic_star[:,4]
-  vy_star        = ic_star[:,5]
-  vz_star        = ic_star[:,6]
+  vx_star        = ic_star[:,4] * vel_normalization
+  vy_star        = ic_star[:,5] * vel_normalization
+  vz_star        = ic_star[:,6] * vel_normalization
+  
   v_star         = np.zeros((3, N_pt_star))
   v_star[0, :]   = vx_star
   v_star[1, :]   = vy_star
@@ -59,9 +72,9 @@ if __name__ == "__main__":
   x_pos_dm       = ic_dm[:,1]
   y_pos_dm       = ic_dm[:,2]
   z_pos_dm       = ic_dm[:,3]
-  vx_dm          = ic_dm[:,4]
-  vy_dm          = ic_dm[:,5]
-  vz_dm          = ic_dm[:,6]
+  vx_dm          = ic_dm[:,4] * vel_normalization
+  vy_dm          = ic_dm[:,5] * vel_normalization
+  vz_dm          = ic_dm[:,6] * vel_normalization
   
   v_dm           = np.zeros((3, N_pt_dm))
   v_dm[0, :]     = vx_dm
@@ -83,19 +96,15 @@ if __name__ == "__main__":
   # ------------------------------------
   # Evolving for the first few time step
   # ------------------------------------
-  
+    
   print("\n")  
   print("Evolution started!!")
-    
-  dt_myr     = 1
-  t_end_myr  = 2.0 
-  f_snap_myr = 1.0 
 
   # get particle level using a tree
   comp_tree = tree(n_dim=n_dim, n_grid=n_grid, n_part_per_cell=n_part_per_cell, mass_pt=mass_star, pos_pt=pos_star)
 
   comp_tree = build_tree_from_particles(tree_in=comp_tree, i_iter_max=10000)
-  acceleration_stars = compute_acceleration_tree(tree_in=comp_tree,critical_angle=0.1, G_gravity= 1.0, n_iter_max=10000, verbose= False)
-  t_now, i_iter, pt_mass, pt_pos, pt_vel, pt_acc, pos_snap, vel_snap, e_snap , t_snap = evolve_to_t_end_galaxy(t_end=t_end_myr, pt_mass=mass_star, pt_pos=pos_star, pt_vel=v_star, pt_acc=acceleration_stars, comp_tree=comp_tree, G_gravity=1.0, max_iterations=1000, n_snap=1000, f_snap=f_snap_myr, eta_time=dt_myr)
+  acceleration_stars = compute_acceleration_tree(tree_in=comp_tree,critical_angle=0.1, G_gravity= G_gravity, n_iter_max=10000, verbose= False)
+  t_now, i_iter, pt_mass, pt_pos, pt_vel, pt_acc, pos_snap, vel_snap, e_snap , t_snap = evolve_to_t_end_galaxy(t_end=t_end_myr, pt_mass=mass_star, pt_pos=pos_star, pt_vel=v_star, pt_acc=acceleration_stars, comp_tree=comp_tree, G_gravity=G_gravity, max_iterations=1e6, n_snap=1000, f_snap=f_snap_myr, eta_time=dt_myr)
     
   
